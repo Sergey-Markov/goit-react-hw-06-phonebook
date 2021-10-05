3ffcbaabccec8f706826b39676c32714231a1665
Sergey-Markov <1nativefl@gmail.com>
2021-10-05 22:34:35 +0300

    delete initialState of contacts

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
-// import { useEffect, useState } from "react";
-// import shortid from "shortid";
 import s from "./App.module.css";
 import Contacts from "./Components/Contacts/Contacts";
 import Filter from "./Components/Filter/Filter";
 import Form from "./Components/Form/Form";
 
 function App() {
-  // const [contacts, setContacts] = useState([
-  //   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  //   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  //   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  //   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  // ]);
-  // const [filter, setFilter] = useState("");
-
-  // useEffect(() => {
-  //   const currentContacts = localStorage.getItem("contacts");
-  //   const parsedCurrentContacts = JSON.parse(currentContacts);
-
-  //   if (parsedCurrentContacts) {
-  //     setContacts(parsedCurrentContacts);
-  //   }
-  // }, []);
-
-  // useEffect(
-  //   (prevState) => {
-  //     if (prevState !== contacts) {
-  //       localStorage.setItem("contacts", JSON.stringify(contacts));
-  //     }
-  //   },
-  //   [contacts]
-  // );
-
   return (
     <div className={s.App}>
       <h1 className={s.title}>Phonebook</h1>
-      {/* <Form onSubmit={addToDataContacts} /> */}
       <Form />
-
       <h2 className={s.title}>Contacts:</h2>
       <Filter />
-
-      {/* <Filter filter={filter} onChange={onFilter} /> */}
-      {/* <Contacts contacts={filteredContact} onClick={deleteContact} /> */}
       <Contacts />
     </div>
   );
--- src/redux/phonebook-reducer.js
+++ src/redux/phonebook-reducer.js
@@ -2,14 +2,7 @@ import { combineReducers } from "redux";
 import { createReducer } from "@reduxjs/toolkit";
 import phonebookActions from "../redux/phonebook-actions";
 
-const initialContactsState = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
-
-const contacts = createReducer(initialContactsState, {
+const contacts = createReducer([], {
   [phonebookActions.addContacts]: (state, { payload }) => {
     if (state.find((contact) => contact.name === payload.name)) {
       alert(`${payload.name} is already created!`);
@@ -54,3 +47,10 @@ export default combineReducers({
 //       return state;
 //   }
 // };
+
+// const initialContactsState = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
